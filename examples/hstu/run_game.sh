a3eb30b777cfad07e66936aa9a4ccbd96c90500f
魏涛 <11186562@MacBook-Pro.local>
2025-11-24 18:58:23 +0800

    add *

--- examples/hstu/run_game.sh
+++ examples/hstu/run_game.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 set -euo pipefail
 
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
@@ -11,12 +12,7 @@ start_ts=$(date +%s)
 start_str=$(date '+%F %T')
 echo "Training start: ${start_str}"
 
-PYTHONPATH=${PYTHONPATH}:$(realpath ../) \
-torchrun --nproc_per_node 8 --nnodes 1 \
-  --node_rank 0 --master_addr localhost --master_port 6000 \
-  ./training/pretrain_gr_retrieval.py \
-  --gin-config-file ./training/configs/gameid_retrieval.gin \
-  2>&1 | tee "${LOG_DIR}/run_game.$(date +%Y%m%d-%H%M%S).log"
+PYTHONPATH=${PYTHONPATH}:$(realpath ../) torchrun --nproc_per_node 8 --nnodes 1 --node_rank 0 --master_addr localhost --master_port 6000 ./training/pretrain_gr_retrieval.py --gin-config-file ./training/configs/gameid_retrieval.gin 2>&1 | tee "${LOG_DIR}/run_game.$(date +%Y%m%d-%H%M%S).log"
 
 end_ts=$(date +%s)
 end_str=$(date '+%F %T')
