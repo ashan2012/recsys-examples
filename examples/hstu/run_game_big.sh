1f85c5a17c443f8af61d823cf45de2d035865096
魏涛 <11186562@MacBook-Pro.local>
2025-11-25 11:58:33 +0800

    add *

--- examples/hstu/run_game_big.sh
+++ examples/hstu/run_game_big.sh
@@ -12,7 +12,7 @@ start_ts=$(date +%s)
 start_str=$(date '+%F %T')
 echo "Training start: ${start_str}"
 
-PYTHONPATH=${PYTHONPATH}:$(realpath ../) torchrun --nproc_per_node 8 --nnodes 1 --node_rank 0 --master_addr localhost --master_port 6000 ./training/pretrain_gr_retrieval.py --gin-config-file ./training/configs/gameid_retrieval.gin 
+PYTHONPATH=${PYTHONPATH}:$(realpath ../) torchrun --nproc_per_node 8 --nnodes 1 --node_rank 0 --master_addr localhost --master_port 6000 ./training/pretrain_gr_retrieval.py --gin-config-file ./training/configs/gameid_retrieval_big.gin 
 
 end_ts=$(date +%s)
 end_str=$(date '+%F %T')
